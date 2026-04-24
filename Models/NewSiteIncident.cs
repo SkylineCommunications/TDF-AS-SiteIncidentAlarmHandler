523c9def2337a3cb5ade816c019942fb00576b0b
lukaPavSkyline <luka.pavlovic@skyline.be>
2026-04-24 10:56:47 +0200

    small fix

--- Models/NewSiteIncident.cs
+++ Models/NewSiteIncident.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Skyline.DataMiner.Automation;
+﻿using Skyline.DataMiner.Automation;
 using Skyline.DataMiner.Net.Messages;
 
 namespace TDFASSiteIncidentAlarmHandler.Models
